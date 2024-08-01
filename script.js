8a2396a3f0f3ba5e01ec02e9c886f77847199e38
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-08-01 21:57:13 +0100

    Logic to get human choice * created input field with prompt method * validate if human input is either rock, paper or scissors * returned human input, which runs only when the above condition is true * Output a message to direct the user with different choice to play with

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+//Logic to get computer choice
 let choice = ["rock", "paper", "scissors"];
-function computerChoice(randomChoice) {
+function getComputerChoice(randomChoice) {
   return (randomChoice = choice[Math.floor(Math.random() * choice.length)]);
 }
-console.log(computerChoice(choice));
+console.log(getComputerChoice(choice));
+
+//Logic to get human choice
+let input = prompt("Play with Rock, Paper or Scissors");
+function getHumanChoice(humanInput) {
+  if (
+    humanInput !== "rock" &&
+    humanInput !== "paper" &&
+    humanInput !== "scissors"
+  ) {
+    console.log("Play with rock, paper or scissors");
+  } else if (humanInput === "") {
+    console.log("Play with rock, paper or scissors");
+  }
+  console.log(humanInput);
+  return humanInput;
+}
+getHumanChoice(input);
